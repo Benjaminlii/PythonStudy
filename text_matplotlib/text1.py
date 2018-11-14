3e698aace06e9ce4fd65a6f6e8e11cb2ff4bdc17
Benjamin <305193188@qq.com>
2018-11-14 18:22:28 +0800

    测试

---
+++ text_matplotlib/text1.py
@@ -0,0 +1,16 @@
+from text_matplotlib.cartogram import *
+from text_matplotlib.get_data_1 import *
+
+x_data = np.random.normal(200, 20, 50000)
+get_histogram(x_data, 15)
+
+x, y_data = get_data("data.xlsx")
+x_data = np.arange(0, x, 1)
+get_line_graph(x_data, y_data, "x1", "y1", "line1")
+
+x_data = np.arange(1, 20, 1)
+y_data_0 = np.cos(x_data)
+y_data_1 = np.sin(x_data)
+get_double_line(x_data, y_data_0, y_data_1, "0", "1", "横轴", "纵轴")
+
+get_curve(np.cos, 10)
