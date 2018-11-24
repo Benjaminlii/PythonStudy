c980287591a4327f9ac9cf6a06e4b48d175f43b7
Benjamin <305193188@qq.com>
2018-11-24 19:44:31 +0800

    更改表 第一行存横标

--- text_pymysql/write_pymysql.py
+++ text_pymysql/write_pymysql.py
@@ -17,22 +17,25 @@ cur = db.cursor()
 for i in range(50):
     # 先编辑MySQL语句
     sql = """create table table%d(
-                n varchar(2),
-                A1 varchar(2),
-                A2 varchar(2),
-                A3 varchar(2),
-                A4 varchar(2),
-                A5 varchar(2),
-                A6 varchar(2),
-                A7 varchar(2),
-                A8 varchar(2)
+                n varchar(10),
+                a1 varchar(10),
+                a2 varchar(10),
+                a3 varchar(10),
+                a4 varchar(10),
+                a5 varchar(10),
+                a6 varchar(10),
+                a7 varchar(10),
+                a8 varchar(10)
             );""" % i
     # 执行语句，下同
     cur.execute(sql)
-    # print(sql)
+    sql = """insert into table%d(n, a1, a2, a3, a4, a5, a6, a7, a8) values (null, "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8");""" % i
+    cur.execute(sql)
     for j in range(1, 9):
         sql = """insert into table%d(n, a1, a2, a3, a4, a5, a6, a7, a8) values (\"%s\", "s", "s", "s", "s", "s", "s", "s", "s");""" % (i, "A"+str(j))
-        # print(sql)
         cur.execute(sql)
+    # 删除50张表
+    # sql = "drop table if exists table%d;" % i
+    # cur.execute(sql)
 db.close()
 
